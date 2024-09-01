36d2a4dc69b8b066153ce8e321362c7189c0b5e5
gamboavalentinabel <gamboavalentinabel@gmail.com>
2024-09-01 19:40:54 -0300

    Add ProfileBot class for logging in and getting cookies

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import ProfileBot from './model/ProfileBot.js'
+
+/**
+ * Instagram Bot Tool.
+ * @param {string} username - The username to profile bot.
+ * @param {string} test - Use this to enable test mode.
+ */
+export default ProfileBot
